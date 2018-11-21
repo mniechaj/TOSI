c7fa052d73899f543e2b865e1fc8e3bba1db5f85
Maciek Niechaj <maciej.niechaj93@gmail.com>
2018-11-21 20:10:09 +0100

    Inital commit. Działający algorytm szyfrowania. Konieczna weryfikacja poprawności.

---
+++ src/main/scala/vernam/Main.scala
@@ -0,0 +1,61 @@
+package vernam
+
+import scala.annotation.tailrec
+import scala.io.Source
+import scala.util.Random
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    val message = Source.fromResource("vernam/message_in").getLines.mkString("\n")
+    val byteMessage = BigInt(message.getBytes)
+    println(s"Original message: $message \n")
+
+    val key = BlumBlumShubGenerator.generateKey(byteMessage.bitLength, 512)
+    val enc = byteMessage ^ key
+    println(s"Encrypted message: $enc \n")
+
+    val dec = key ^ enc
+    println(s"Decrypted message: ${new String(dec.toByteArray)} \n")
+  }
+
+}
+
+object BlumBlumShubGenerator {
+  def generateKey(bitLength: Int, pqBitLength: Int): BigInt = {
+    val rnd = new Random()
+    val p = randomPrime(pqBitLength, rnd)
+    val q = randomPrime(pqBitLength, rnd)
+    val n = p * q
+    val seed = generateSeed(pqBitLength * 2, rnd, n)
+    val x0 = seed.pow(2) mod n
+    internalGenerateKey(bitLength, 0, x0, n, BigInt(bitLength, new Random()))
+  }
+
+  @tailrec
+  private def randomPrime(bitLength: Int, rnd: Random): BigInt = {
+    val p = BigInt.probablePrime(bitLength, rnd)
+    if (p.mod(4) == 3) p
+    else randomPrime(bitLength, rnd)
+  }
+
+  @tailrec
+  private def generateSeed(bitLength: Int, rnd: Random, n: BigInt): BigInt = {
+    val seed = BigInt.probablePrime(bitLength, rnd)
+    if (seed.gcd(n) == 1) seed
+    else generateSeed(bitLength, rnd, n)
+  }
+
+  @tailrec
+  private def internalGenerateKey(bitLength: Int, ctr: Int, prevX: BigInt, n: BigInt, key: BigInt): BigInt = {
+    if (ctr == bitLength) key
+    else {
+      val x = prevX.pow(2) mod n
+      val k = x mod 2
+
+      if (k == 1) internalGenerateKey(bitLength, ctr + 1, x, n, key.setBit(ctr))
+      else internalGenerateKey(bitLength, ctr + 1, x, n, key.clearBit(ctr))
+    }
+  }
+
+}
